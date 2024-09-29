151566573c5518b2dc092584ec6c42a56f3a127e
HomeMedia <taknado13@yandex.ru>
2024-09-29 16:57:26 +0700

    поправил х2

--- src/main/java/ru/hogwarts/school/service/PortServiceTest.java
+++ src/main/java/ru/hogwarts/school/service/PortServiceTest.java
@@ -1,19 +1,18 @@
-package ru.hogwarts.school.service;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Service;
-
-@Service
-@Profile("test")
-
-public class PortServiceTest implements PortService {
-    @Value("${server.port}")
-    private String port;
-
-    @Override
-    public String portInfo() {
-        return port;
-    }
-
-}
+//package ru.hogwarts.school.service;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Profile;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//
+//public class PortServiceTest implements PortService {
+//    @Value("${server.port}")
+//    private String port;
+//
+//    @Override
+//    public String portInfo() {
+//        return port;
+//    }
+//
+//}
